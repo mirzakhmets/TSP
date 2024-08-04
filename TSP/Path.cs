2e8de667f77c7ff01a030fa9663e4423dfc03fab
mirzakhmets <mirzakhmets@icloud.com>
2024-08-04 13:10:50 +0500

    Release

---
+++ TSP/Path.cs
@@ -0,0 +1,119 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: mspma
+ * Date: 6/9/2024
+ * Time: 3:42 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections;
+
+namespace TSP
+{
+	/// <summary>
+	/// Path candidate.
+	/// </summary>
+	public class Path
+	{
+		public static int N;
+		
+		public static int [,] W;
+		
+		public static int[] Optimal, Current;
+		
+		public static Random random = new Random(System.DateTime.Now.Millisecond);
+		
+		public static int PathLength (int[] path) {
+			int result = 0;
+			
+			for (int i = 1; i < path.Length; ++i) {
+				result += W[path[i - 1], path[i]];
+			}
+			
+			return result;
+		}
+		
+		public static void Initialize (int N, int MaxLength) {
+			Path.N = N;
+			
+			W = new int[N, N];
+			
+			for (int i = 0; i < N; ++i) {
+				for (int j = 0; j < N; ++j) {
+					W[i, j] = random.Next(MaxLength);
+				}
+			}
+			
+			Optimal = new int[N];
+			
+			Current = new int[N];
+			
+			for (int i = 0; i < N; ++i) {
+				Optimal[i] = i;
+				
+				Current[i] = i;
+			}
+			
+			Permute(0, N - 1);
+		}
+		
+		public static void Permute(int l, int r) {
+			if (l == r) {
+				if (PathLength(Current) < PathLength(Optimal)) {
+					for (int i = 0; i < N; ++i) {
+						Optimal[i] = Current[i];
+					}
+				}
+			} else {
+				for (int i = l; i <= r; ++i) {
+					int k = Current[i];
+					Current[i] = Current[l];
+					Current[l] = k;
+					
+					Permute(l + 1, r);
+					
+					k = Current[i];
+					Current[i] = Current[l];
+					Current[l] = k;
+				}
+			}
+		}
+		
+		public ArrayList items = new ArrayList();
+		
+		public Path()
+		{
+		}
+		
+		public bool Contains(int v) {
+			for (int i = 0; i < items.Count; ++i) {
+				if ((int) items[i] == v) {
+					return true;
+				}
+			}
+			
+			return false;
+		}
+		
+		public int Length() {
+			int result = 0;
+			
+			for (int i = 1; i < items.Count; ++i) {
+				result += W[(int) items[i - 1], (int) items[i]];
+			}
+			
+			return result;
+		}
+		
+		public void Reverse() {
+			this.items.Reverse();
+		}
+		
+		public void Merge(Path path) {
+			for (int i = 0; i < path.items.Count; ++i) {
+				this.items.Add(path.items[i]);
+			}
+		}
+	}
+}
---
+++ TSP/Program.cs
@@ -0,0 +1,142 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: mspma
+ * Date: 6/9/2024
+ * Time: 3:42 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TSP
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Path.Initialize(10, 2000);
+			
+			Console.WriteLine(Path.PathLength(Path.Optimal));
+			
+			for (int i = 0; i < Path.N; ++i) {
+				Console.Write(Path.Optimal[i] + " ");
+			}
+			
+			Console.WriteLine();
+			
+			List<Path> current = new List<Path>();
+			
+			for (int i = 0; i < Path.N; ++i) {
+				Path p = new Path();
+				
+				p.items.Add(i);
+				
+				current.Add(p);
+			}
+			
+			while (current.Count > 1) {
+				Path nPath = null;
+				
+				Path pa = null, pb = null;
+				
+				for (int i = 0; i < current.Count; ++i) {
+					for (int j = 0; j < current.Count; ++j) {
+						if (i != j) {
+							for (int k = 0; k < 2; ++k) {
+								Path a = (Path) current[i];
+								
+								for (int l = 0; l < 2; ++l) {
+									Path b = (Path) current[j];
+									
+									Path z = new Path();
+									
+									z.Merge(a);
+									
+									z.Merge(b);
+									
+									bool cont = false;
+									
+									do {
+										cont = false;
+										
+										for (int pi = 0; pi < z.items.Count; ++pi) {
+											for (int pj = pi + 1; pj < z.items.Count; ++pj) {
+												int lk = z.Length();
+												
+												int e = (int) z.items[pi];
+													
+												z.items[pi] = z.items[pj];
+													
+												z.items[pj] = e;
+												
+												if (lk > z.Length()) {													
+													cont = true;
+													
+													goto aab;
+												}
+												
+												e = (int) z.items[pi];
+													
+												z.items[pi] = z.items[pj];
+													
+												z.items[pj] = e;																								
+											}
+										}
+										
+									aab:
+										;
+									} while (cont);
+									
+									if (nPath == null || nPath.Length() > z.Length()) {
+										nPath = z;
+										
+										pa = current[i];
+										
+										pb = current[j];
+									}
+									
+									b.Reverse();
+								}
+								
+								a.Reverse();
+							}
+						}
+					}
+				}
+				
+				foreach (Path p in current) {
+					Console.WriteLine(p.Length());
+					
+					for (int i = 0; i < p.items.Count; ++i) {
+						Console.Write(p.items[i] + " ");
+					}
+					
+					Console.WriteLine();
+					
+					Console.WriteLine("--");
+				}
+				
+				Console.WriteLine();
+				
+				if (nPath != null) {
+					current.Add(nPath);
+					
+					if (pa != null) {
+						current.Remove(pa);
+					}
+					
+					if (pb != null) {
+						current.Remove(pb);
+					}
+				}
+			}
+			
+			Console.WriteLine(current[0].Length());
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
